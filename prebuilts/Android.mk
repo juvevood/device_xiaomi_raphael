f7496ca8eb00750f7921962f31c74ba9f253fd19
juvevood <juvemail8@gmail.com>
2019-09-17 11:35:21 +0800

    add mipay sepolicy & support wechat alipay fp

--- prebuilts/Android.mk
+++ prebuilts/Android.mk
@@ -10,3 +10,14 @@ LOCAL_OVERRIDES_PACKAGES := Home Launcher2 Launcher3 Launcher3QuickStep
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SoterService
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := priv-app/SoterService/SoterService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
